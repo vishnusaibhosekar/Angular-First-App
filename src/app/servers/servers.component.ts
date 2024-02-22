9f9c707eaa3ac44fee2b054b255f8ec1754c4b1b
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2024-02-22 14:59:41 -0800

    adds files

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css',
 })
 export class ServersComponent {
   allowNewServer = false;
