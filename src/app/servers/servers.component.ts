c08ad9be57dea9f08cc39d0a07b169107517efe6
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2024-02-22 13:40:33 -0800

    adds files

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css',
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'Click to create new Server';
+  placeholderServerName = 'Enter Server Name';
+  serverName = 'Test Server';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    if (this.serverName === '') {
+      this.serverCreationStatus = 'Enter a valid name';
+      return;
+    }
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
